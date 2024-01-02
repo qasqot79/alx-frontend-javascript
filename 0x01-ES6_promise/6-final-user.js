caae2a57632f606ceb342d7a4851ad96d1252345
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-02 20:24:42 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => {
-      return results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
+  /* eslint-disable */
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      const profilearray = [];
+      result.forEach((index) => {
+	if (index.status !== 'fulfilled') {
+	  profilearray.push({ status: index.status, value: `Error: ${index.reason.message}` });
+	} else {
+	  profilearray.push({ status: index.status, value: index.value });
+	}
+      });
+      return profilearray;
     });
 }
