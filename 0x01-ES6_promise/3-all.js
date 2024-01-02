940e318cb7f909df16784c4125a7b0a3459f6a88
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-02 20:21:04 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,8 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from '../alx-frontend-javascript/utils.js'
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch((error) => {
-      console.error('Signup system offline');
-    });
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
