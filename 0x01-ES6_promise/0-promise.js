76ef937233ff7e51ebdbecfcc611017078738ce6
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-02 20:18:36 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
-function getResponseFromAPI() {
+export default function getRespondFromAPI() {
+  /* eslint-disable */
   return new Promise((resolve, reject) => {
-    // Your asynchronous API call or any other async operation goes here
-    // For demonstration purposes, let's resolve the promise after a short timeout
-    setTimeout(() => {
-      resolve('API response data');
-    }, 1000);
+      if (true) {
+	resolve();
+      } else {
+	reject();
+      }
   });
 }
-
-export default getResponseFromAPI;
