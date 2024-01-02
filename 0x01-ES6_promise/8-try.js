c466cafee5f024f4e7723d537a2a560ef437b911
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-02 20:27:23 +0100

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    return numerator / denominator;
-  } catch (error) {
-    throw error;
+  /* eslint-disable */
+  if (denominator !== 0) {
+    return (numerator / denominator);
   }
+  throw new Error('cannot divide by 0');
 }
