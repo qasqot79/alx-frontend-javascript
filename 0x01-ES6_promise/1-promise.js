366f55f58a6602b5e6857516c50226b444c6179e
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-02 20:14:36 +0100

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,13 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromApi(success) {
+  /* eslint-disable */
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
+    if (!success) {
       reject(new Error('The fake API is not working currently'));
+    } else {
+      resolve({
+	status: 200,
+	body: 'Success',
+      });
     }
   });
 }
-
-export default getFullResponseFromAPI;
