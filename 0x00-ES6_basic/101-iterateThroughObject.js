41fbabea2e05db6c93a8918f049d2c29fe69c127
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-03 13:55:26 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,14 @@
-export default function createReportObject(employeesList) {
-  /* eslint-disable */
-  return {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+
+  for (const employee of reportWithIterator) {
+    result += employee + ' | ';
+  }
+
+  // Remove the trailing ' | ' if there are employees
+  if (result.length > 0) {
+    result = result.slice(0, -3);
+  }
+
+  return result;
 }
