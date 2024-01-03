a9859a0c4cc69167ea9e98c5fe5b5e895ec87256
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-03 14:17:57 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,15 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-
-  for (const employee of reportWithIterator) {
-    result += employee + ' | ';
-  }
-
-  // Remove the trailing ' | ' if there are employees
-  if (result.length > 0) {
-    result = result.slice(0, -3);
-  }
-
-  return result;
+  return [...reportWithIterator].join(' | ');
 }
-
