f3f782595ae67a892daa0a21e52a7a80145c9a87
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-04 17:01:37 +0100

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,50 +1,36 @@
 import Currency from './3-currency';
 
 export default class Pricing {
+  /* eslint-disable */
   constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    } else if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of class Currency');
-    }
-
-    this._amount = amount;
     this._currency = currency;
+    this._amount = amount;
+  }
+
+  // getter 
+  get currency() {
+    return this._currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  get currency() {
-    return this._currency;
+  // setter
+  set currency(Currency) {
+    this._currency = Currency;
   }
 
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = amount;
-  }
-
-  set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be an instance of class Currency');
-    }
-    this._currency = currency;
+  set amount(Amount) {
+    this._amount = Amount;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`; 
   }
 
+  // static that calculates the amount multiplied by the conversion rate
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    } else if (typeof conversionRate !== 'number') {
-      throw new TypeError('Conversion rate must be a number');
-    }
-
     return amount * conversionRate;
   }
 }
