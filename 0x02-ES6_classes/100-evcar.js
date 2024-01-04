a1fa5b6e5881246c6bdb03dd7fc2a1d3af2d1653
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-04 16:20:19 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,32 @@
-import Car from './10-car';
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const CarClass = this.constructor;
+    const clonedCar = new CarClass();
+    Object.assign(clonedCar, this);
+    return clonedCar;
+  }
+}
+
+import Car from './10-car.js';
 
 class EVCar extends Car {
-  constructor(brand = '', motor = '', color = '', range = '') {
+  constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  cloneCar() {
+    const CarClass = super.constructor;
+    const clonedCar = new CarClass();
+    Object.assign(clonedCar, this);
+    return clonedCar;
   }
 }
 
+export default EVCar;
