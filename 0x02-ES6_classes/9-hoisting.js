852a1d137445b0becb4f5405f8a6638f48384d9a
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-04 17:09:48 +0100

    Update 9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,5 +1,4 @@
 export class HolbertonClass {
-  /* eslint-disable */
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -15,7 +14,6 @@ export class HolbertonClass {
 }
 
 export class StudentHolberton {
-  /* eslint-disable */
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -31,18 +29,10 @@ export class StudentHolberton {
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
+export const listOfStudents = [];
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-const listOfStudents = [student1, student2, student3, student4, student5];
-export default listOfStudents;
+   
