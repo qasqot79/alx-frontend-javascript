c3160898293d0adff9cddba70256315c8a71716b
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-04 16:16:55 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,19 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
 
-  static get [Symbol.species]() {
-    return this;
+class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
   cloneCar() {
-    const ModelCar = this.constructor[Symbol.species];
-    return new ModelCar();
+    const CarClass = this.constructor;
+    const clonedCar = new CarClass();
+    return Object.assign(clonedCar, this);
   }
+}
+
+export default Car;
