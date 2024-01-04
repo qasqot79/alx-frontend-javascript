8b682e7f2bb481f68ee5ac593269705085b0087a
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-04 16:56:14 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,30 +1,14 @@
-export default class Building() {
-  /* eslint-disable */
-  export default class Building() {
-  /* eslint-disable */
-  constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+export default class Building {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
-    if (
-      this.constructor !== Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw Error(
-	'Class extending Building must override evacuationWarningMessage',
-      );
     }
 
     this._sqft = sqft;
   }
 
-  // getter
   get sqft() {
     return this._sqft;
-  }
-
-  // setter 
-  set sqft(newSqft) {
-    this._sqft = newSqft;
   }
 }
