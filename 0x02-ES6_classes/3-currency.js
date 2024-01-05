fab9a48d25610aa005233fbc01bbd0dd564f0129
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-05 12:06:24 +0100

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,36 +1,38 @@
-import Currency from './3-currency';
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
 
-export default class Pricing {
-  /* eslint-disable */
-  constructor(amount, currency) {
-    this._currency = currency;
-    this._amount = amount;
+    this._code = code;
+    this._name = name;
   }
 
-  // getter 
-  get currency() {
-    return this._currency;
+  get name() {
+    return this._name;
   }
 
-  get amount() {
-    return this._amount;
+  get code() {
+    return this._code;
   }
 
-  // setter
-  set currency(Currency) {
-    this._currency = Currency;
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
   }
 
-  set amount(Amount) {
-    this._amount = Amount;
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
   }
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`; 
-  }
-
-  // static that calculates the amount multiplied by the conversion rate
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
