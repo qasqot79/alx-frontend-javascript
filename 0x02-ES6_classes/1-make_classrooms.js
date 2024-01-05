147b1fc85f7d1f727c35f67aee81cc2c7ea20d85
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-05 12:10:05 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  /* eslint-disable */
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  ClassRoomList.push(class1, class2, class3);
-  return ClassRoomList;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
