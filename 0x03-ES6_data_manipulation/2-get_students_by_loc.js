35d41abdf3324e5f21e1159fea0c90a82d79bfac
Risikat Popoola <111513209+qasqot79@users.noreply.github.com>
2024-01-08 20:02:21 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation(students, city) {
-  return students.filter((obj) => obj.location === city);
+function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
+
+export default getStudentsByLocation;
